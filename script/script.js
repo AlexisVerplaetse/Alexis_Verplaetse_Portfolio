9fa5f6c7d9f5f3226ac0a644d029af1ed693d6a2
Alexis <alexis.verplaetse1@gmail.com>
2026-03-10 16:24:08 +0100

    changement de css et ajout de bouton scroll

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// Script JavaScript pour le portfolio
+//import './bootstrap';
+// Fonction pour remonter tous en haut de la page
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Animation de défilement fluide
+    });
+}
