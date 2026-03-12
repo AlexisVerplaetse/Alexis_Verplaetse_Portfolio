7f5d616233340e173209f4fd9e4a0a54c14c97f1
Alexis <alexisverplaetse1@gmail.com>
2026-03-12 18:11:07 +0100

    ajout du projet apeeac stage

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 // Script JavaScript pour le portfolio
-// Fonction pour remonter tous en haut de la page
+// Fonction pour remonter tout en haut de la page
 
 function scrollToTop() {
     window.scrollTo({
@@ -8,11 +8,25 @@ function scrollToTop() {
     });
 }
 
-// modal functions
+// ── MODAL FUNCTIONS ─────────────────────
+
+// ouvrir le modal
 window.openModal = function(id){
   document.getElementById(id).style.display = "flex";
 }
 
+// fermer le modal
 window.closeModal = function(id){
   document.getElementById(id).style.display = "none";
 }
+
+// fermer le modal si on clique en dehors
+window.addEventListener("click", function(event) {
+  const modals = document.querySelectorAll(".modal");
+
+  modals.forEach(function(modal){
+    if(event.target === modal){
+      modal.style.display = "none";
+    }
+  });
+});
